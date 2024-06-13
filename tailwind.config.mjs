1193aeae4b0f59e5df4494648659d952d5f09a8d
Eleni Koundouraki <eleni@oxygenna.com>
2024-06-13 11:51:47 +0300

    Updated tailwind config
    - Added colours, cursor and font family
    - Added typography plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        primary: {
+          50: "#fef2f2",
+          100: "#fde3e3",
+          200: "#fdcbcb",
+          300: "#faa7a7",
+          400: "#f47575",
+          500: "#ec5959",
+          600: "#d72b2b",
+          700: "#b52020",
+          800: "#951f1f",
+          900: "#7c2020",
+          950: "#430c0c",
+        },
+        neutral: {
+          50: "#f8fafc",
+          100: "#f1f5f9",
+          200: "#e2e8f0",
+          300: "#cbd5e1",
+          400: "#94a3b8",
+          500: "#64748b",
+          600: "#475569",
+          700: "#334155",
+          800: "#1e293b",
+          900: "#0f172a",
+          950: "##020617",
+        },
+      },
+      cursor: {
+        fancy: "url(https://www.svgrepo.com/show/269/color-picker.svg)",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
