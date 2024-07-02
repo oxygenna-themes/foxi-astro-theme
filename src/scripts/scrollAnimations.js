9548e9b07d75b4c5325f340b24cdbdc7a58a2240
Christos Pantazis <christos@oxygenna.com>
2024-07-02 12:00:17 +0300

    added page transitions

--- src/scripts/scrollAnimations.js
+++ src/scripts/scrollAnimations.js
@@ -1,13 +1,24 @@
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('scroll-animated')
-		}
+const initializeObserver = () => {
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('scroll-animated')
+			}
+		})
 	})
-})
 
-const elements = document.querySelectorAll('.scroll-animation .col')
+	const elements = document.querySelectorAll('.scroll-animation .col')
 
-elements.forEach((element) => {
-	observer.observe(element)
-})
+	elements.forEach((element) => {
+		observer.observe(element)
+	})
+}
+
+// Initialize observer on initial load
+initializeObserver()
+
+// Hook into Astro View Transitions
+document.addEventListener('astro:page-loaded', initializeObserver)
+
+// This event should be triggered after the new content is loaded
+document.addEventListener('astro:after-swap', initializeObserver)
