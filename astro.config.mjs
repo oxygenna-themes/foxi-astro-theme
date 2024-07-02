c994f70bccabd1600845fce7efdb2047b82532d8
Christos Pantazis <christos@oxygenna.com>
2024-07-02 09:24:33 +0300

    added sitemap, fixed og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  // site: 'site domain here',
+  integrations: [tailwind(), icon(), sitemap()],
 });
