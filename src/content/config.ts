f47c571dd49a3a32a1bac9c71b95614ff2b6b09a
Eleni Koundouraki <eleni@oxygenna.com>
2024-07-04 11:15:07 +0300

    Created config.ts for collections

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection } from 'astro:content'
+
+const blogCollection = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		pubDate: z.date(),
+		description: z.string(),
+		author: z.string(),
+		image: z.object({
+			url: z.string(),
+			alt: z.string()
+		}),
+		tags: z.array(z.string())
+	})
+})
+
+export const collections = {
+	posts: blogCollection
+}
