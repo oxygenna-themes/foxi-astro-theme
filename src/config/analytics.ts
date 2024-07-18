94f936ee8ac38b262e1c2cb969bd10446febc885
Christos Pantazis <christos@oxygenna.com>
2024-07-18 14:21:28 +0300

    added analytics scripts

---
+++ src/config/analytics.ts
@@ -0,0 +1,8 @@
+// Google tracking
+// - Google Site Verification: for Google Search Console
+// - Google Analytics Measurement ID: for Google Analytics
+// - Google Tag Manager ID: for Google Tag Manager
+
+export const googleSiteVerification = ''
+export const googleAnalyticsMeasurementID = ''
+export const googleTagManagerID = ''
