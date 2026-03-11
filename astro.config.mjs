ee4481822de2ee43adfa90a7356def88ff72945d
Eleni Koundouraki <3171175+chrispantazis@users.noreply.github.com>
2026-03-11 13:04:39 +0200

    Updated to Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://foxi.netlify.app",
   integrations: [
     icon(),
     sitemap(),
